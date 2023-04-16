1075b9ef661b3069772374bca447ac8d62df3c08
Fabio-Oliveira-Dev <fabiooliveiraideia2@gmail.com>
2023-04-16 17:30:48 -0300

    gerando novos pokemons atraves de botão

--- Modulo_2/Projeto_pokedex/assets/js/main.js
+++ Modulo_2/Projeto_pokedex/assets/js/main.js
@@ -1,29 +1,44 @@
-
-// Conversão de um pokemon para item de lista
-function convertPokemonToLi(pokemon) {
-    return `
-    <li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
-
-        <div class="detail">
-            <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-            </ol>
-
-            <img src="${pokemon.photo}" alt="${pokemon.name}">
-        </div>
-    </li>
-    `
-}
-
 // Pagando a lista de pokemons e atribuindo a uma variavel
 const pokemonList = document.getElementById('pokemonList')
 
-// interface de comunicação de uma promisse
-// Vai retornar uma promisse requisição assincrona
-// Pegar a lista de pokemons, mapear e converter em lista html, juntar tudo e sem separador
-pokeApi.getPokemons().then((pokemons = []) => {
-    const newHTML = pokemons.map(convertPokemonToLi).join('')
-    pokemonList.innerHTML = newHTML
+// Criando botão de paginação
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const limit = 5
+let offset = 0;
+
+
+
+function loadPokemonItens(offset, limit) {
+    // interface de comunicação de uma promisse
+    // Vai retornar uma promisse requisição assincrona
+    // Pegar a lista de pokemons, mapear e converter em lista html, juntar tudo e sem separador
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHTML = pokemons.map((pokemon) => 
+        // Conversão de um pokemon para item de lista
+        `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+        
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+        
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>
+        `).join('')
+
+        pokemonList.innerHTML += newHTML
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+// Evento de quando clicar no botão de peginação
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
 })
