8ba0812a468a00b3039fffc46555fb003ee859d0
Fabio-Oliveira-Dev <fabiooliveiraideia2@gmail.com>
2023-08-09 23:10:09 -0300

    atualização do arquivo .doc Modulo_3

---
+++ Modulo_3/variaveis.js
@@ -0,0 +1,24 @@
+// Declaração e variável
+// var num = 10;
+// var texto = 'Ola Programador';
+
+// console.log('O seu texto foi ', texto, 'e o número escolhido foi', num);
+
+
+//Hoisting
+// teste();
+// function teste(){
+//     console.log('Teste')
+// };
+
+// if(true){
+//     var var1 = 10;
+// }
+
+// function teste(){
+//     var var1 = 20;
+// }
+
+// teste();
+// console.log(var1);
+
